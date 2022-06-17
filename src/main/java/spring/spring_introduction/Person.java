233a7fe6f46acdfa9a9b628e94deb8eb99d489e3
YurySkosyrev <skosyrev-87@mail.ru>
2022-06-17 16:04:03 +0300

    Step 12/102

--- src/main/java/spring/spring_introduction/Person.java
+++ src/main/java/spring/spring_introduction/Person.java
@@ -2,6 +2,8 @@ package spring.spring_introduction;
 
 public class Person {
     private Pet pet;
+    private String surname;
+    private int age;
     public Person(){
         System.out.println("Person bean is created");
     }
@@ -13,7 +15,25 @@ public class Person {
 
     public void setPet(Pet pet) {
         this.pet = pet;
-        System.out.println("Pet is setted");
+        System.out.println("Class Person: set pet");
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        System.out.println("Class Person: set surname");
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        System.out.println("Class Person: set age");
+        this.age = age;
     }
 
     public void callYourPet(){
