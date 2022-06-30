beb26d535762206826c678f15e215d394a140673
YurySkosyrev <skosyrev-87@mail.ru>
2022-06-30 22:59:15 +0300

    Step 59/102

--- MVC/src/main/Java/com/spring/mvc/MyController.java
+++ MVC/src/main/Java/com/spring/mvc/MyController.java
@@ -1,7 +1,10 @@
 package com.spring.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class MyController {
@@ -10,4 +13,26 @@ public class MyController {
     public String showFirstView(){
         return "first-view";
     }
+
+    @RequestMapping("/askDetails")
+    public String askEmployeeDetails(){
+        return "ask-emp-details-view";
+    }
+
+//    @RequestMapping("/showDetails")
+//    public String showEmpDetails(){
+//        return "show-emp-details-view";
+//    }
+
+    @RequestMapping("/showDetails")
+    public String showEmpDetails(HttpServletRequest request, Model model){
+
+        String empName = request.getParameter("employeeName");
+        empName = "Mr. " + empName;
+        model.addAttribute("nameAttribute", empName);
+        model.addAttribute("description", "- udemy instructor");
+
+        return "show-emp-details-view";
+    }
+
 }
---
+++ MVC/src/main/webapp/WEB-INF/view/show-emp-details-view.jsp
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+<h2>Dear employee, your are wellcome</h2>
+<br>
+<br>
+<br>
+
+<%--Your name: ${param.employeeName}--%>
+
+Your name: ${nameAttribute} ${description}
+
+</body>
+
+</html>
