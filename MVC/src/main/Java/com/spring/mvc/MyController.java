0e170ab45c08ba9fa7620509098256c75b53921f
YurySkosyrev <skosyrev-87@mail.ru>
2022-07-01 14:43:26 +0300

    Step 61/102

--- MVC/src/main/Java/com/spring/mvc/MyController.java
+++ MVC/src/main/Java/com/spring/mvc/MyController.java
@@ -3,10 +3,12 @@ package com.spring.mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
+@RequestMapping("/employee")
 public class MyController {
 
     @RequestMapping("/")
@@ -19,19 +21,12 @@ public class MyController {
         return "ask-emp-details-view";
     }
 
-//    @RequestMapping("/showDetails")
-//    public String showEmpDetails(){
-//        return "show-emp-details-view";
-//    }
 
     @RequestMapping("/showDetails")
-    public String showEmpDetails(HttpServletRequest request, Model model){
+    public String showEmpDetails(@RequestParam("employeeName") String empName, Model model){
 
-        String empName = request.getParameter("employeeName");
         empName = "Mr. " + empName;
         model.addAttribute("nameAttribute", empName);
-        model.addAttribute("description", "- udemy instructor");
-
         return "show-emp-details-view";
     }
 
