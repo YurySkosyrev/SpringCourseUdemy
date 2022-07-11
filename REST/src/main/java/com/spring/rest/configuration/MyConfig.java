9d5eea799a738d28ee868cfe5485dd5edfa7e544
Aviaurez <skosyrev-87@mail.ru>
2022-07-12 00:00:21 +0300

    Step 81/102

--- REST/src/main/java/com/spring/rest/configuration/MyConfig.java
+++ REST/src/main/java/com/spring/rest/configuration/MyConfig.java
@@ -1,11 +1,8 @@
 package com.spring.rest.configuration;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
-import com.sun.tools.sjavac.ProblemException;
-import org.hibernate.SessionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
@@ -13,12 +10,11 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.sql.DataSource;
-import javax.xml.bind.PropertyException;
 import java.beans.PropertyVetoException;
 import java.util.Properties;
 
 @Configuration
-@ComponentScan(basePackages = "com.spring.rest.configuration")
+@ComponentScan(basePackages = "com.spring.rest")
 @EnableWebMvc
 @EnableTransactionManagement
 public class MyConfig {
@@ -31,21 +27,23 @@ public class MyConfig {
             dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/my_db?useSSL=false&amp;serverTimezone=UTC");
             dataSource.setUser("bestuser");
             dataSource.setPassword("bestuser");
-        } catch (PropertyVetoException e) {
-            e.printStackTrace();
-        }
 
+        } catch (PropertyVetoException e) {
+            throw new RuntimeException(e);
+        }
         return dataSource;
     }
 
     @Bean
     public LocalSessionFactoryBean sessionFactory(){
+
         LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
 
         sessionFactory.setDataSource(dataSource());
         sessionFactory.setPackagesToScan("com.spring.rest.entity");
 
         Properties hibernateProperties = new Properties();
+
         hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
         hibernateProperties.setProperty("hibernate.show_sql", "true");
 
@@ -56,10 +54,13 @@ public class MyConfig {
 
     @Bean
     public HibernateTransactionManager transactionManager(){
-        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        HibernateTransactionManager transactionManager =
+                new HibernateTransactionManager();
 
         transactionManager.setSessionFactory(sessionFactory().getObject());
 
         return transactionManager;
     }
+
+
 }
