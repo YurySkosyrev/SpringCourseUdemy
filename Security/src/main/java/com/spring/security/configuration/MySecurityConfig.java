c53017e6552a4d928e9f6ff95772c559c42c6ab9
YurySkosyrev <skosyrev-87@mail.ru>
2022-07-13 09:49:16 +0300

    Step 90/102

---
+++ Security/src/main/java/com/spring/security/configuration/MySecurityConfig.java
@@ -0,0 +1,19 @@
+package com.spring.security.configuration;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+@EnableWebSecurity
+public class MySecurityConfig extends WebSecurityConfigurerAdapter{
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .withUser(userBuilder.username("Yury").password("Yury").roles("EMPLOYEE"))
+                .withUser(userBuilder.username("Elena").password("Elena").roles("HR"))
+                .withUser(userBuilder.username("Ivan").password("Ivan").roles("MANAGER", "HR"));
+    }
+}
