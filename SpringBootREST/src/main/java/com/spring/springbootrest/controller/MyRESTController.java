cd61ba6508ae83b2c892a7bff4a02814bccbe1a9
YurySkosyrev <skosyrev-87@mail.ru>
2022-07-16 22:56:02 +0300

    Step 98/102

--- SpringBootREST/src/main/java/com/spring/springbootrest/controller/MyRESTController.java
+++ SpringBootREST/src/main/java/com/spring/springbootrest/controller/MyRESTController.java
@@ -2,6 +2,7 @@ package com.spring.springbootrest.controller;
 
 
 import com.spring.springbootrest.entity.Employee;
+import com.spring.springbootrest.exception_handling.NoSuchEmployeeException;
 import com.spring.springbootrest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -23,33 +24,33 @@ public class MyRESTController {
         return allEmployees;
     }
 
-//    @GetMapping("/employees/{id}")
-//    public Employee getEmployee(@PathVariable int id){
-//        Employee employee = employeeService.getEmployee(id);
-//
-//        return employee;
-//    }
-//
-//    @PostMapping("/employees")
-//    public Employee addNewEmployee(@RequestBody Employee employee){
-//        employeeService.saveEmployee(employee);
-//        return employee;
-//    }
-//
-//    @PutMapping("/employees")
-//    public Employee updateEmployee(@RequestBody Employee employee){
-//        employeeService.saveEmployee(employee);
-//        return employee;
-//    }
-//
-//    @DeleteMapping("/employees/{id}")
-//    public String deleteEmployee(@PathVariable int id){
-//
-//        Employee employee = employeeService.getEmployee(id);
-//        if (employee==null) {
-//            throw new NoSuchEmployeeException("There is no employee with ID "+ id + " in Database");
-//        }
-//        employeeService.deleteEmployee(id);
-//        return "Employee with id " + id + " was deleted";
-//    }
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable int id){
+        Employee employee = employeeService.getEmployee(id);
+
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addNewEmployee(@RequestBody Employee employee){
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee){
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public String deleteEmployee(@PathVariable int id){
+
+        Employee employee = employeeService.getEmployee(id);
+        if (employee==null) {
+            throw new NoSuchEmployeeException("There is no employee with ID "+ id + " in Database");
+        }
+        employeeService.deleteEmployee(id);
+        return "Employee with id " + id + " was deleted";
+    }
 }
---
+++ SpringBootREST/src/main/java/com/spring/springbootrest/exception_handling/NoSuchEmployeeException.java
@@ -0,0 +1,8 @@
+package com.spring.springbootrest.exception_handling;
+
+public class NoSuchEmployeeException extends RuntimeException{
+
+    public NoSuchEmployeeException(String message) {
+        super(message);
+    }
+}
