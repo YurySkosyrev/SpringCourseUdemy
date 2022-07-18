814d68ada62133f953f2cd5c5f6d02f1920a65a9
Aviaurez <skosyrev-87@mail.ru>
2022-07-18 21:46:50 +0300

    Step 102/102 - mission accomplished, good work

---
+++ SpringDataRest/src/main/java/com/springcourse/springdatarest/dao/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.springcourse.springdatarest.dao;
+
+
+
+import com.springcourse.springdatarest.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository <Employee, Integer> {
+}
